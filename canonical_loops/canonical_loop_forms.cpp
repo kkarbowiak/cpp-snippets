99ec9d1938ca1be038582bb518652aac8145850f
kkarbowiak <kkarbowiak@localhost>
2016-12-19 23:50:21 +0100

    Rewrite canonical loop forms as compilable code

--- canonical_loops/canonical_loop_forms.cpp
+++ canonical_loops/canonical_loop_forms.cpp
@@ -1,13 +1,48 @@
-vsize_t n = 0;
-while (n != v.size())
+#include <vector>
+#include <iostream>
+
+
+typedef std::vector<int> vi_t;
+
+void print(int i)
 {
-    // Process v[n]
-    ++n;
+    std::cout << i << std::endl;
 }
 
-vsize_t n = v.size();
-while (n != 0)
+void printUp(vi_t const & v)
 {
-    --n;
-    // Process v[n]
+    vi_t::size_type n = 0;
+    while (n != v.size())
+    {
+        print(v[n]); // Process v[n]
+
+        ++n;
+    }
 }
+
+void printDown(vi_t const & v)
+{
+    vi_t::size_type n = v.size();
+    while (n != 0)
+    {
+        --n;
+
+        print(v[n]); // Process v[n]
+    }
+}
+
+int main()
+{
+    vi_t v;
+
+    for (int i = 1; i <= 5; ++i)
+    {
+        v.push_back(i);
+    }
+
+    printUp(v);
+    printDown(v);
+
+    return 0;
+}
+
