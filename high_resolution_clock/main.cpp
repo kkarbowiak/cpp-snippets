bfddb5bd458fef2bfd9cc038c467d575f94359be
kkarbowiak <kkarbowiak@localhost>
2017-06-27 12:52:08 +0200

    Minor cleanup: remove using directive

--- high_resolution_clock/main.cpp
+++ high_resolution_clock/main.cpp
@@ -3,7 +3,6 @@
 
 int main()
 {
-    using namespace std;
     using namespace std::chrono;
 
     auto const t1 = high_resolution_clock::now();
@@ -13,12 +12,12 @@ int main()
     auto const t5 = high_resolution_clock::now();
     auto const t6 = high_resolution_clock::now();
 
-    cout << time_point_cast<nanoseconds>(t1).time_since_epoch().count() << std::endl;
-    cout << time_point_cast<nanoseconds>(t2).time_since_epoch().count() << std::endl;
-    cout << time_point_cast<nanoseconds>(t3).time_since_epoch().count() << std::endl;
-    cout << time_point_cast<nanoseconds>(t4).time_since_epoch().count() << std::endl;
-    cout << time_point_cast<nanoseconds>(t5).time_since_epoch().count() << std::endl;
-    cout << time_point_cast<nanoseconds>(t6).time_since_epoch().count() << std::endl;
+    std::cout << time_point_cast<nanoseconds>(t1).time_since_epoch().count() << std::endl;
+    std::cout << time_point_cast<nanoseconds>(t2).time_since_epoch().count() << std::endl;
+    std::cout << time_point_cast<nanoseconds>(t3).time_since_epoch().count() << std::endl;
+    std::cout << time_point_cast<nanoseconds>(t4).time_since_epoch().count() << std::endl;
+    std::cout << time_point_cast<nanoseconds>(t5).time_since_epoch().count() << std::endl;
+    std::cout << time_point_cast<nanoseconds>(t6).time_since_epoch().count() << std::endl;
 
     return 0;
 }
