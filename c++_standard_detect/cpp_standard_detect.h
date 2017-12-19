cdd31704da20c83aaed9c81ab48509f6b563f25f
kkarbowiak <kkarbowiak@localhost>
2017-12-19 15:04:43 +0100

    Add preprocessor defines for detecting supported C++ standard version

---
+++ c++_standard_detect/cpp_standard_detect.h
@@ -0,0 +1,26 @@
+#ifndef CPP_STD_DETECT_H
+#define CPP_STD_DETECT_H
+
+#if defined(_MSVC_LANG)
+    #if _MSVC_LANG >= 201703
+        #define CPP_17_PRESENT
+    #elif _MSVC_LANG >= 201402
+        #define CPP_14_PRESENT
+    #else
+        #define CPP_11_PRESENT
+    #endif
+#elif defined(__cplusplus)
+    #if __cplusplus >= 201703L
+        #define CPP_17_PRESENT
+    #elif __cplusplus >= 201402L
+        #define CPP_14_PRESENT
+    #elif __cplusplus >= 201103L
+        #define CPP_11_PRESENT
+    #elif __cplusplus >= 199711L
+        #define CPP_98_PRESENT
+    #elif __cplusplus == 1L
+        #define CPP_PRE98_PRESENT
+    #endif
+#endif
+
+#endif /* CPP_STD_DETECT_H */
